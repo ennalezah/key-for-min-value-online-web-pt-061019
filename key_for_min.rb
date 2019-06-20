f398a2dcb17332ad80562121ddc23c31fb43c416
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-06-20 02:08:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min = ""
+  value_array = []
   
   
   name_hash.collect do |key, value|
-    
+    value_array << value
   end 
 
 end
